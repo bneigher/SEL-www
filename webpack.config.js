f18c53b95db227c3a8d368358f2fc871d0b5762d
bneigher <bneigher@gmail.com>
2019-10-23 20:16:47 -0700

    added DNS:

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   output: {
     path: buildDir,
     filename: isProd ? "bundle.[chunkhash].js" : "bundle.js",
-    publicPath: isProd ? "/SEL-www/" : "/"
+    publicPath: isProd ? "/" : "/"
   },
   module: {
     rules: [
